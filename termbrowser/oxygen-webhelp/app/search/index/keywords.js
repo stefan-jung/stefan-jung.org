6b0f28b08128d5104166c4777d7d33266a0da245
Stefan Jung <stefan.jung@Dometic.com>
2024-08-15 08:33:55 +0200

    Add termbrowser.

---
+++ termbrowser/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,45 @@
+var keywords=[{w:"Advanced",p:["p0"]},{w:"driver",p:["p0"]},{w:"assistance",p:["p0"]},{w:"systems",p:["p0"]},{w:"Anti-lock",p:["p1"]},{w:"breaking",p:["p1"]},{w:"system",p:["p1","p16"]},{w:"Axle",p:["p2"]},{w:"Volkswagen",p:["p3"]},{w:"Beetle",p:["p3"]},{w:"Brake",p:["p4"]},{w:"Light",p:["p4"]},{w:"Bus",p:["p5"]},{w:"Car",p:["p6"]},{w:"Chassis",p:["p7"]},{w:"Class",p:["p8","p9","p10"]},{w:"B",p:["p8"]},{w:"motorhome",p:["p8","p9","p10","p24"]},{w:"B+",p:["p9"]},{w:"C",p:["p10"]},{w:"Coachwork",p:["p11"]},{w:"(vehicle",p:["p11"]},{w:"body)",p:["p11"]},{w:"Context",p:["p12"]},{w:"Sensitive",p:["p12"]},{w:"Help",p:["p12"]},{w:"Diesel",p:["p13"]},{w:"engine",p:["p13","p15","p17","p19","p26"]},{w:"Electronic",p:["p14"]},{w:"stability",p:["p14"]},{w:"control",p:["p14"]},{w:"Exhaust",p:["p16"]},{w:"Gas",p:["p17"]},{w:"Headlamp",p:["p18"]},{w:"Internal",p:["p19"]},{w:"combustion",p:["p19"]},{w:"K\u00E4ssbohrer",p:["p20"]},{w:"Setra",p:["p20"]},{w:"S",p:["p20"]},{w:"110",p:["p20"]},{w:"H",p:["p20"]},{w:"Ladder",p:["p21"]},{w:"frame",p:["p21"]},{w:"Lighting",p:["p22"]},{w:"Motorcycle",p:["p23"]},{w:"Motor",p:["p25"]},{w:"vehicle",p:["p25","p37"]},{w:"Petrol",p:["p26"]},{w:"(spark-ignition)",p:["p26"]},{w:"Powertrain",p:["p27"]},{w:"Rear",p:["p28","p29"]},{w:"fog",p:["p28"]},{w:"lamp",p:["p28","p29","p30"]},{w:"position",p:["p29"]},{w:"Reversing",p:["p30"]},{w:"Semantic",p:["p31"]},{w:"Net",p:["p31"]},{w:"Terminology",p:["p32"]},{w:"statistics",p:["p32"]},{w:"Transmission",p:["p33"]},{w:"Truck",p:["p34"]},{w:"Turn",p:["p35"]},{w:"signal",p:["p35"]},{w:"Unitized",p:["p36"]},{w:"body",p:["p36"]},{w:"Wheel",p:["p38"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[4, 5, 6];
+ph["p2"]=[7];
+ph["p3"]=[8, 9];
+ph["p4"]=[10, 11];
+ph["p5"]=[12];
+ph["p6"]=[13];
+ph["p7"]=[14];
+ph["p8"]=[15, 16, 17];
+ph["p9"]=[15, 18, 17];
+ph["p30"]=[54, 52];
+ph["p10"]=[15, 19, 17];
+ph["p32"]=[57, 58];
+ph["p31"]=[55, 56];
+ph["p12"]=[23, 24, 25];
+ph["p34"]=[60];
+ph["p11"]=[20, 21, 22];
+ph["p33"]=[59];
+ph["p14"]=[28, 29, 30];
+ph["p36"]=[63, 64];
+ph["p13"]=[26, 27];
+ph["p35"]=[61, 62];
+ph["p16"]=[31, 6];
+ph["p38"]=[65];
+ph["p15"]=[27];
+ph["p37"]=[46];
+ph["p18"]=[33];
+ph["p17"]=[32, 27];
+ph["p19"]=[34, 35, 27];
+ph["p21"]=[41, 42];
+ph["p20"]=[36, 37, 38, 39, 40];
+ph["p23"]=[44];
+ph["p22"]=[43];
+ph["p25"]=[45, 46];
+ph["p24"]=[17];
+ph["p27"]=[49];
+ph["p26"]=[47, 27, 48];
+ph["p29"]=[50, 53, 52];
+ph["p28"]=[50, 51, 52];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
